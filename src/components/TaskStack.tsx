c52efd3c3c50643278c1a40df8509b18bd3f0a61
Kenji Phang <ken.pcx@outlook.com>
2023-09-06 04:02:43 +0100

    Added core logic

--- src/components/TaskStack.tsx
+++ src/components/TaskStack.tsx
@@ -1,23 +1,79 @@
-import { ScrollArea, Stack } from "@mantine/core";
+import { Alert, Box, Image, ScrollArea, Stack, Text } from "@mantine/core";
 import { useState } from "react";
+import { AppTask } from "../types";
+
+const mockTasks = [
+  { task: "A", priority: 1 },
+  { task: "B", priority: 2 },
+  { task: "C", priority: 33 },
+  { task: "D", priority: 4 },
+];
+
+const mockBacklog = [
+  { task: "AA", priority: 1 },
+  { task: "BV", priority: 2 },
+  { task: "CD", priority: 33 },
+  { task: "DS", priority: 4 },
+];
 
 const TaskStack = () => {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState<AppTask[]>(mockTasks);
+  const [backlog, setBacklog] = useState<AppTask[]>([]);
 
-  const pushToStack = (task: string, priority: number = 0) => {};
+  const pushTaskToStack = (task: string, priority: number = 0) => {
+    setTasks((oldTasks) => {
+      oldTasks.unshift({ task, priority });
+      oldTasks.sort((a, b) => b.priority - a.priority);
+      return oldTasks;
+    });
+  };
 
-  const popFromStack = () => {};
+  const popTaskFromStack = () => {
+    if (tasks.length == 0) return;
+    setTasks((oldTasks) => oldTasks.slice(1));
+  };
 
-  const backlog = (task: string) => {};
+  const backlogTask = (task: string, priority: number = 0) => {
+    setBacklog((oldBacklog) => {
+      oldBacklog.unshift({ task, priority });
+      oldBacklog.sort((a, b) => b.priority - a.priority);
+      return oldBacklog;
+    });
+  };
 
-  const clearStack = () => {};
+  const popTaskFromBacklog = () => {
+    if (backlog.length == 0) return;
+    setTasks((oldBacklog) => oldBacklog.slice(1));
+  };
 
-  const clearBacklog = () => {};
+  const clearStack = () => {
+    setTasks([]);
+  };
+
+  const clearBacklog = () => {
+    setBacklog([]);
+  };
 
   return (
-    <ScrollArea>
-      <Stack h={"100%"} align="center" justify="center"></Stack>
-    </ScrollArea>
+    <Stack h={"100%"} align="center" justify="center">
+      {tasks.length > 0 ? (
+        tasks.map((task, i) => <div>{task.task}</div>)
+      ) : backlog.length > 0 ? (
+        backlog.map((backlog, i) => <div>{backlog.task}</div>)
+      ) : (
+        <Box>
+          <Image
+            maw={250}
+            mb={50}
+            mx="auto"
+            radius="md"
+            src="/relax.jpg"
+            alt="Random image"
+          />
+          <Alert>Empty tasks and backlog. Add some!</Alert>
+        </Box>
+      )}
+    </Stack>
   );
 };
 
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export type AppTask = {
+  task: string;
+  priority: number;
+};
