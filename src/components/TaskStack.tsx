5740403fb96c185ca401e4628c9856e99a13ca24
Kenji Phang <ken.pcx@outlook.com>
2023-09-06 04:37:21 +0100

    Integrated Azure STT

--- src/components/TaskStack.tsx
+++ src/components/TaskStack.tsx
@@ -1,6 +1,19 @@
-import { Alert, Box, Image, ScrollArea, Stack, Text } from "@mantine/core";
-import { useState } from "react";
+import {
+  Alert,
+  Box,
+  Button,
+  Image,
+  Stack,
+  Text,
+} from "@mantine/core";
+import { useEffect, useState } from "react";
 import { AppTask } from "../types";
+import {
+  SpeechConfig,
+  SpeechRecognizer,
+  AudioConfig,
+} from "microsoft-cognitiveservices-speech-sdk";
+import { IconMicrophone } from "@tabler/icons-react";
 
 const mockTasks = [
   { task: "A", priority: 1 },
@@ -16,9 +29,33 @@ const mockBacklog = [
   { task: "DS", priority: 4 },
 ];
 
+const speechConfig = SpeechConfig.fromSubscription(
+  import.meta.env.VITE_AZURE_SPEECH_RECOGNITION_KEY,
+  import.meta.env.VITE_AZURE_REGION
+);
+speechConfig.speechRecognitionLanguage = "en-US";
+
 const TaskStack = () => {
   const [tasks, setTasks] = useState<AppTask[]>(mockTasks);
   const [backlog, setBacklog] = useState<AppTask[]>([]);
+  const [transcript, setTranscript] = useState<string>("");
+  const [transcriptVisible, setTranscriptVisible] = useState<boolean>(false);
+  const [recording, setRecording] = useState<boolean>(false);
+
+  const recognizeSpeech = () => {
+    setRecording(true);
+    const audioConfig = AudioConfig.fromDefaultMicrophoneInput();
+    const speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+    speechRecognizer.recognizeOnceAsync(
+      (res) => {
+        setTranscript(res.text);
+        setRecording(false);
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  };
 
   const pushTaskToStack = (task: string, priority: number = 0) => {
     setTasks((oldTasks) => {
@@ -54,6 +91,22 @@ const TaskStack = () => {
     setBacklog([]);
   };
 
+  useEffect(() => {
+    if (transcript === "") return;
+
+    const fadeIn = setTimeout(() => {
+      setTranscriptVisible(true);
+    }, 100);
+    const fadeOut = setTimeout(() => {
+      setTranscriptVisible(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(fadeIn);
+      clearTimeout(fadeOut);
+    };
+  }, [transcript]);
+
   return (
     <Stack h={"100%"} align="center" justify="center">
       {tasks.length > 0 ? (
@@ -73,6 +126,27 @@ const TaskStack = () => {
           <Alert>Empty tasks and backlog. Add some!</Alert>
         </Box>
       )}
+      <Stack sx={{ position: "absolute", bottom: "10%" }}>
+        <Text
+          color="dimmed"
+          sx={{
+            textAlign: "center",
+            opacity: transcriptVisible ? 1 : 0,
+            transition: "opacity 500ms",
+          }}
+        >
+          {transcript}
+        </Text>
+        <Button
+          leftIcon={<IconMicrophone />}
+          size="lg"
+          onClick={recognizeSpeech}
+          disabled={recording}
+          variant="light"
+        >
+          Record Command
+        </Button>
+      </Stack>
     </Stack>
   );
 };
