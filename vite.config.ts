1007053f3404dd3b6a0599475248383ede96b653
Kenji Phang <ken.pcx@outlook.com>
2023-09-07 04:47:08 +0100

    Configured PWA settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+const manifestForPlugIn: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icon-180x180.png", "maskable-icon-512x512.png"],
+  manifest: {
+    name: "MindStack",
+    short_name: "MindStack",
+    description: "Keep track of your virtual stack",
+    icons: [
+      {
+        src: "pwa-64x64.png",
+        sizes: "64x64",
+        type: "image/png",
+      },
+      {
+        src: "pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "maskable-icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(manifestForPlugIn)],
+});
